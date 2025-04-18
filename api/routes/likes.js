91be9eb6497d9a347219238f16d6d5ff46e80830
devashree <devashree.masurkar@softlabsgroup.com>
2025-04-18 12:47:16 +0530

    Add like route and connect it to the likePost controller

--- api/routes/likes.js
+++ api/routes/likes.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { likePost } from '../controllers/like.js';
+import { VERIFY_ACCESS_TOKEN } from '../middleware/jwtAuth.js';
 const router = express.Router();
 
-
+router.post("/addLike",VERIFY_ACCESS_TOKEN, likePost);
 
 
 export default router
