c8edc2b1b71a7072f5eae13079e33bbfa139dbb8
devashree <devashree.masurkar@softlabsgroup.com>
2025-04-21 16:49:58 +0530

    Update comments route handling

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { addComments, deleteComment, getComments } from '../controllers/comment.js';
+import { VERIFY_ACCESS_TOKEN } from '../middleware/jwtAuth.js';
 const router = express.Router();
 
-
+router.post("/:postId/comment", VERIFY_ACCESS_TOKEN, addComments);
+router.get("/:postId/comments", VERIFY_ACCESS_TOKEN, getComments);
+router.delete("/:commentId", VERIFY_ACCESS_TOKEN, deleteComment);
 
 
 export default router
