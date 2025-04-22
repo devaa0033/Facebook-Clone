01babc5e83dd7cb309afdd97dbfbd402cdad0e13
devashree <devashree.masurkar@softlabsgroup.com>
2025-04-22 17:58:04 +0530

    Fix follow count logic in follow.js

--- api/routes/follow.js
+++ api/routes/follow.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { followController, getFollowers, getFollowing, getFollowSuggestion, isFollowing, unfollowController } from '../controllers/followController.js';
+import { followController, getFollowCounts, getFollowers, getFollowing, getFollowSuggestion, isFollowing, unfollowController } from '../controllers/followController.js';
 import { VERIFY_ACCESS_TOKEN } from '../middleware/jwtAuth.js';
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get("/folloers/:userId", VERIFY_ACCESS_TOKEN ,getFollowers);
 router.get("/following/:userId", VERIFY_ACCESS_TOKEN ,getFollowing);
 router.get("/isFollowing/:targetUserId", VERIFY_ACCESS_TOKEN ,isFollowing);
 router.get("/suggestions", VERIFY_ACCESS_TOKEN ,getFollowSuggestion);
+router.get("/follow-counts/:userId", VERIFY_ACCESS_TOKEN ,getFollowCounts);
 
 export default router
